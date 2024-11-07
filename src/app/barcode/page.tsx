7047876f9d671fa49ca758a2506a949579a8fade
brunoblaise <brunoblaise94@gmail.com>
2024-11-07 21:57:13 +0200

    Qrcode scanner

---
+++ src/app/barcode/page.tsx
@@ -0,0 +1,32 @@
+
+"use client"
+import React, { FC, useRef, useState } from 'react'
+import { QrReader, QrReaderProps } from 'react-qr-reader';
+
+const Bar:FC =() => {
+ 
+    const [data, setData] = useState('No result');
+
+
+  return (
+    <div>
+      <QrReader 
+      className="lg:h-[400px] lg:w-[400px] h-[300px] w-[300px]"
+      onResult={(result:any, error:any) => {
+        if (!!result) {
+          setData(result?.text);
+        }
+
+        if (!!error) {
+          console.info(error);
+        }
+      }}
+  
+      constraints={{ facingMode: "environment" }}
+      />
+      <p>{data}</p>
+    </div>
+  )
+}
+
+export default Bar;
